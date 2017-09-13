8164cd29e7c2194eca008de2a06890bc813ea95d
AJC <aaron_carass@jhu.edu>
2017-09-13 14:36:03 -0400

    Updated.

--- lab_1b/submission.py
+++ lab_1b/submission.py
@@ -6,13 +6,19 @@
 import random
 
 
-from playground.network.packet import PacketType
+from playground.network.packet import PacketType, FIELD_NOT_SET
 from playground.network.packet.fieldtypes import BOOL, UINT32, STRING, BUFFER
+from playground.network.packet.fieldtypes import StringFieldType
+from playground.network.packet.fieldtypes.attributes import Optional
 
 
 class RequestMathQuestion(PacketType):
     DEFINITION_IDENTIFIER = "RMQ"
     DEFINITION_VERSION = "1.0"
+
+    FIELDS = [
+    		("Type", STRING)
+    		]
 
 
 class MathQuestion(PacketType):
@@ -21,7 +27,7 @@ class MathQuestion(PacketType):
 
     FIELDS = [
             ("O1", UINT32),
-            ("O2", UINT32),
+            ("O2", UINT32({Optional:True})),
             ("Operation", STRING),
             ("ID", UINT32)
             ]
@@ -58,90 +64,49 @@ class MathError(PacketType):
             ]
 
 
+
+def testPacketType(pt):
+	packet = pt()
+
+	if packet.DEFINITION_IDENTIFIER == "RMQ":
+		packet.Type = "Problem"
+	elif packet.DEFINITION_IDENTIFIER == "MQ":
+		packet.O1 = RI()
+		packet.O2 = RI()
+		packet.Operation = "add"
+		packet.ID = RI()
+	elif packet.DEFINITION_IDENTIFIER == "MS":
+		packet.Solution = random.randint(0, 2**32)
+		packet.ID = RI()
+	elif packet.DEFINITION_IDENTIFIER == "MR":
+		packet.Result = bool(random.getrandbits(1))
+		packet.ID = RI()
+	elif packet.DEFINITION_IDENTIFIER == "ME":
+		packet.ID = RI()
+
+
+	packetByte = packet.__serialize__()
+	testPacket = pt().Deserialize(packetByte)
+
+	assert packet == testPacket
+
+
+
+def RI():
+	return random.randint(0, 2**32)
+
+
+
 def basicUnitTest():
-    #
-    # p1 is a packet of type RequestMathQuestion()
-    #
-    # t1 is the test deserialized equivalent packet of p1
-    #
-    p1 = RequestMathQuestion()
-
-    p1b = p1.__serialize__()
-
-    t1 = RequestMathQuestion.Deserialize(p1b)
-
-    assert p1 == t1
-
-
-
-    #
-    # p2 is a packet of type MathQuestion()
-    #
-    # t2 is the test deserialized equivalent packet of p2
-    #
-    p2 = MathQuestion()
-    p2.O1 = random.randint(0, 2^32)
-    p2.O2 = random.randint(0, 2^32)
-    p2.Operation = "add"
-    p2.ID = random.randint(0, 2^32)
-
-    p2b = p2.__serialize__()
-
-    t2 = MathQuestion.Deserialize(p2b)
-
-    assert p2 == t2
-
-
-
-    #
-    # p3 is a packet of type MathSolution()
-    #
-    # t3 is the test deserialized equivalent packet of p3
-    #
-    p3 = MathSolution()
-    p3.Solution = random.randint(0, 2^32)
-    p3.ID = random.randint(0, 2^32)
-
-    p3b = p3.__serialize__()
-
-    t3 = MathSolution.Deserialize(p3b)
-
-    assert p3 == t3
-
-
-    #
-    # p4 is a packet of type MathResult()
-    #
-    # t4 is the test deserialized equivalent packet of p4
-    #
-    p4 = MathResult()
-    p4.Result = bool(random.getrandbits(1))
-    p4.ID = random.randint(0, 2^32)
-
-    p4b = p4.__serialize__()
-
-    t4 = MathResult.Deserialize(p4b)
-
-    assert p4 == t4
-
-
-    #
-    # p5 is a packet of type MathError()
-    #
-    # t5 is the test deserialized equivalent packet of p5
-    #
-    p5 = MathResult()
-    p5.Result = bool(random.getrandbits(1))
-    p5.ID = random.randint(0, 2^32)
-
-    p5b = p5.__serialize__()
-
-    t5 = MathResult.Deserialize(p5b)
-
-    assert p5 == t5
+	for i in range(1, (2**5)):
+		testPacketType(RequestMathQuestion)
+		testPacketType(MathQuestion)
+		testPacketType(MathSolution)
+		testPacketType(MathResult)
+		testPacketType(MathError)
 
 
 
 if __name__ == "__main__":
-    basicUnitTest()
+	basicUnitTest()
 
